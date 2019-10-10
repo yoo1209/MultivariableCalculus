f5907cd3117f3043a13638fd5e15f0717262dd47
Hyosang Kang <hyosang@dgist.ac.kr>
2019-10-10 12:20:09 +0900

    Update week06.m

--- Weekly/Recitation/week06.m
+++ Weekly/Recitation/week06.m
@@ -8,19 +8,29 @@ X(u,v) = [ sin(u)*cos(v), sin(u)*sin(v), cos(u) ];
 Xu = diff(X,u);
 Xv = diff(X,v);
 F(u,v) = subs(f,[x,y,z],X(u,v));
-F = matlabFunction(F(u,v));
-integral2(F, pi/2-acos(1/2), pi/2+acos(1/2), 0, 2*pi)
+aF = matlabFunction(F(u,v)*norm(cross(Xu,Xv)));
+integral2(aF, pi/2-acos(1/2), pi/2+acos(1/2), 0, 2*pi)
 
 %% Problem2
+clear all
+syms x y z u v
 [X,Y] = meshgrid(linspace(-1,1,10));
 surf(X,Y,X.^2-Y.^2); hold on;
 quiver3(X,Y,X.^2-Y.^2,X.*Y,exp(Y),X.^2+Y); hold off;
 
+clear X Y
+F(x,y,z) = [x*y, exp(y), x^2+y];
+X(u,v) = [u, v, u^2-v^2];
+FX(u,v) = subs(F,[x,y,z],X);
+FXu = diff(FX, u);
+FXv = diff(FX, v);
+f = matlabFunction(dot(FX(u,v), cross(FXu(u,v),FXv(u,v))));
+integral2(f, -1, 1, -1, 1)
+
 %% Computing line integrals
 
+
 %% Problem 1
-
-
 function V = sort_vert(v)
     x0 = mean(v(:,1));
     y0 = mean(v(:,2));
@@ -42,8 +52,6 @@ end
 
 
 %% Problem 2
-
-
 function A = area_conv(v)
     A = 0;
     if size(v, 1) < 3
