685d456e542f23d03ca7a02d41910ae491d16892
Hyosang Kang <hyosang@dgist.ac.kr>
2019-11-07 15:47:26 +0900

    Update week10.m

--- Weekly/Recitation/week10.m
+++ Weekly/Recitation/week10.m
@@ -1,7 +1,9 @@
 %% intersection of the sphere with a saddle
 
+clear all
+
 [x,y,z] = sphere(50);
-surf(x,y,z); hold on;
+surf(x,y,z,'facealpha',.5); hold on;
 [ t, r ] = meshgrid( linspace(0, 2*pi, 50), linspace(0, 1, 50) );
 surf( r.*cos(t), r.*sin(t), r.^2.*cos(t).^2 - r.^2.*sin(t).^2 ); 
 
@@ -12,7 +14,7 @@ plot3(sin(theta).*cos(phi), sin(theta).*sin(phi), cos(theta), 'r', 'linewidth', 
 axis equal;
 
 syms x y z r t p 
-F(x,y,z) = [ x^2, y, sin(z) ];
+F(x,y,z) = [ x^2*y, cos(y)+x^2, y^2+sin(z) ];
 X1(r,t,p) = [ r*sin(t)*cos(p), r*sin(t)*sin(p), r*cos(t) ];
 X1(t,p) = subs( X1(r,t,p), r, 1 );
 FX1(t,p) = subs( F(x,y,z), [x,y,z], X1(t,p) );
@@ -25,10 +27,16 @@ X2(r,t) = subs( X2(r,t,z), z, r^2*cos(2*t) );
 FX2(r,t) = subs( F(x,y,z), [x,y,z], X2(r,t) );
 f = matlabFunction( dot( FX2(r,t), cross( diff(X2(r,t),r), diff(X2(r,t),t) ) ), 'Vars', [t,r] );
 rmax = @(t) sqrt( (sqrt(1+4*cos(2*t).^2) -1) ./ (2*cos(2*t).^2) );
-b = integral2(f,0,2*pi,0,rmax)
+b = integral2(f, 0, 2*pi, 0, rmax)
 
-X(r,t,p) = [ r*sin(t)*cos(p), r*sin(t)*sin(p), r*cos(t) ];
-J = simplify( dot( cross(diff(X(r,t,p),r), diff(X(r,t,p),t)), diff(X(r,t,p),p) ) );
-f = matlabFunction( subs( divergence(F(x,y,z)), [x,y,z], X(r,t,p) ) * J, 'Vars', [r,p,t] );
-tmax = @(r,p) acos( ( -1 + sqrt( 1 + 4*cos(2*p).^2 ) ) ./ ( 2*cos(2*p) ) ) ;
-c = integral3(f,0,1,0,2*pi,0,tmax)
+X(r,t,z) = [ r*cos(t), r*sin(t), z ];
+J = simplify( dot( cross(diff(X(r,t,z),r), diff(X(r,t,z),t)), diff(X(r,t,z),z) ) );
+f = matlabFunction( subs( divergence(F(x,y,z)), [x,y,z], X(r,t,z) ) * J, 'Vars', [t,r,z] );
+zmin = @(t,r) r.^2.*cos(2*t);
+zmax = @(t,r) sqrt(1 - r.^2.*cos(t).^2 - r.^2.*sin(t).^2);
+zmaxminus = @(t,r) -sqrt(1 - r.^2.*cos(t).^2 - r.^2.*sin(t).^2);
+c = real(...
+    integral3(f, 0, 2*pi, 0, rmax, zmin, zmax) + ...
+    integral3(f, pi/4, 3*pi/4, rmax, 1, zmaxminus, zmax) + ...
+    integral3(f, 5*pi/4, 7*pi/4, rmax, 1, zmaxminus, zmax))
+a-b
